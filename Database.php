0de8b445a5a6fb9086977790caebace19eaa147c
mrprotocoll <lekanvgbg@gmail.com>
2023-09-12 20:51:24 +0100

    install dotenv

--- Database.php
+++ Database.php
@@ -1,15 +1,19 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+// Load environment variables from .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-    class Database {
-        protected $conn;
-        public function getConnection(){
-            $this->conn = null;
-            try{
-                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database_name, $this->username, $this->password);
-                $this->conn->exec("set names utf8");
-            }catch(PDOException $exception){
-                echo "Database could not be connected: " . $exception->getMessage();
-            }
-            return $this->conn;
+class Database {
+    protected $conn;
+    public function getConnection(){
+        $this->conn = null;
+        try{
+            $this->conn = new PDO("mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_NAME'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+            $this->conn->exec("set names utf8");
+        }catch(\PDOException $exception){
+            echo "Database could not be connected: " . $exception->getMessage();
         }
+        return $this->conn;
     }
+}
